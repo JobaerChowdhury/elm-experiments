d98b0ae4190efcba4d7094396ef25adbf7df2ee1
jobaer <jobaer.du@gmail.com>
2016-08-06 13:22:04 +0600

    Show the current time, without formatting

--- Main.elm
+++ Main.elm
@@ -1,8 +1,77 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, div, text)
+import Html.App as App
+import Time exposing (Time, second)
+import Task exposing (perform)
 
 
-main : Html.Html a
+main : Program Never
 main =
-    text "Hello, World!"
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Time
+
+
+getTime : Cmd Msg
+getTime =
+    Time.now |> Task.perform (\x -> NoOp) Tick
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, getTime )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+    | Tick Time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick newTime ->
+            ( newTime, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+--subscriptions : Model -> Sub Msg
+--subscriptions model =
+--Time.every second Tick
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "Current time is: "
+        , text (toString model)
+        ]
